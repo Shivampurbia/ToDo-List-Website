03a2d1fd95bd94e216c526b7c2a7fd416cfd7cb3
Saket Kothari <saketkothari722@gmail.com>
2021-12-30 10:27:23 +0530

    adding new todos on user input

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,68 @@ function main() {
           : './assets/images/icon-sun.svg'
       );
     });
+
+  // get alltodos and initialise listeners
+  addTodo();
+  // dragover on .todos container
+  document.querySelector('.todos').addEventListener('dragover', function (e) {
+    e.preventDefault();
+    if (
+      !e.target.classList.contains('dragging') &&
+      e.target.classList.contains('card')
+    ) {
+      const draggingCard = document.querySelector('.dragging');
+      const cards = [...this.querySelectorAll('.card')];
+      const currPos = cards.indexOf(draggingCard);
+      const newPos = cards.indexOf(e.target);
+      console.log(currPos, newPos);
+
+      if (currPos > newPos) {
+        this.insertBefore(draggingCard, e.target);
+      } else {
+        this.insertBefore(draggingCard, e.target.nextSibling);
+      }
+      const todos = JSON.parse(localStorage.getItem('todos'));
+      const removed = todos.splice(currPos, 1);
+      todos.splice(newPos, 0, removed[0]);
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  });
+
+  // add new todos on user input
+  const add = document.getElementById('add-btn');
+  const txtInput = document.querySelector('.txt-input');
+  add.addEventListener('click', function () {
+    const item = txtInput.value.trim();
+    if (item) {
+      txtInput.value = '';
+      const todos = !localStorage.getItem('todos')
+        ? []
+        : JSON.parse(localStorage.getItem('todos'));
+      const currentTodo = {
+        item,
+        isCompleted: false,
+      };
+      addTodo([currentTodo]);
+      todos.push(currentTodo);
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+    txtInput.focus();
+  });
+
+  // add todo also on enter key event
+  txtInput.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      add.click();
+    }
+  });
+  // filter todo - all, active, completed
+  document.querySelector('.filter').addEventListener('click', function (e) {
+    const id = e.target.id;
+    if (id) {
+      document.querySelector('.on').classList.remove('on');
+      document.getElementById(id).classList.add('on');
+      document.querySelector('.todos').className = `todos ${id}`;
+    }
+  });
 }
